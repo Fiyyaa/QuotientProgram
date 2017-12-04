af8d512d46436db228753d4396968f7606e33b79
Fiyyaa <smacrito@gmail.com>
2017-12-04 15:45:10 -0600

    Final

--- Quotient Program.py
+++ Quotient Program.py
@@ -1,22 +1,25 @@
 def divideBySubtraction():
-    dividend = float(0)
+    dividend = float(0) #define all variables.. Had error before with declaring
+#all floats on one line, so I did them one by one
     divisor = float(0)
     quotient = float(0)
     remainder = float(0)
     newDividend = float(0)
     count = float(0)
-    dividend = float(input("Please enter a dividend: "))
+    dividend = float(input("Please enter a dividend: ")) #Takes users input
     divisor = float(input("Now enter a divisor: "))
-    newDividend = dividend
-    #print(newDividend)
-    while (newDividend >= divisor):
+    newDividend = dividend #Declares newDividend so it can be in the scope
+#of the loop
+    #print(newDividend) <-- old debugging
+    while (newDividend >= divisor): #will stop once newDividend is less than
+#divisor
         remainder = newDividend - divisor
         newDividend = remainder
-        print(newDividend)
+        #print(newDividend) <-- old debugging
         count += 1
         
     quotient = count
     print(str(dividend) + " divided by " + str(divisor) + " = " + str(quotient)
-          + " remainder " + str(remainder))
+          + " remainder " + str(remainder)) #concantates and prints
     
-divideBySubtraction()
+divideBySubtraction() #calls function
